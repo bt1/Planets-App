fbe443c61c0307d5f9ef20a73e0b9dae05176b73
BennyT <btan@computer.org>
2014-08-11 22:34:09 -0400

    Initial Commit

---
+++ Out of this World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Benny Tan on 8/11/14.
+//  Copyright (c) 2014 Squarevibe Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+    }
+}
